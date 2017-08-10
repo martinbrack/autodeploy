c98a181a2ddb05a72065c821caf3319c2c8f6c85
Martin Brack <martin.brack@kommunal.at>
2017-08-10 17:04:19 +0200

    Pulled pork

---
+++ payload.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Endpoint for Github Webhook URLs
+ *
+ * see: https://help.github.com/articles/post-receive-hooks
+ *
+ */
+
+// script errors will be send to this email:
+$error_mail = "martin.brack@kommunal.at";
+
+function run() {
+    global $rawInput;
+
+
+    $postBody = $_POST['payload'];
+
+    $file = "list.txt";
+
+
+    file_put_contents($file, $postBody);
+}
+
+try {
+    if (!isset($_POST['payload'])) {
+        echo "Works fine.";
+    } else {
+        run();
+    }
+} catch ( Exception $e ) {
+    $msg = $e->getMessage();
+    mail($error_mail, $msg, ''.$e);
+}
